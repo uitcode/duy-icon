259f680b4f95dd0a5f440a96ab8fa634c51296f0
Duc Duy <0374740747s@gmail.com>
2021-02-17 11:47:29 +0700

    change ver

--- public-api.d.ts
+++ public-api.d.ts
@@ -1,5 +1,6 @@
-export * from './lib/registry-icons.service';
-export * from './lib/icons.component';
-export * from './lib/icons.module';
-export * from './lib/icons.service';
+export * from "./lib/registry-icons.service";
+export * from "./lib/icons.component";
+export * from "./lib/icons.module";
+export * from "./lib/icons.service";
+export * from "./icons/index";
 //# sourceMappingURL=public-api.d.ts.map
